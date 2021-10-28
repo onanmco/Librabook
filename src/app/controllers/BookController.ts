23447bb3e9143035a33a0dc084700b2133e9f021
onanmco <onanmco@gmail.com>
2021-10-28 20:29:36 +0300

    [enhancement] updated get /books to allow sfetching book list sorted by reading rate via a query param called sort

--- src/app/controllers/BookController.ts
+++ src/app/controllers/BookController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { query, Request, Response } from 'express';
 import {
   REST_CONTROLLER,
   HTTP_GET,
@@ -7,8 +7,14 @@ import { Book } from '../entities/Book';
 
 @REST_CONTROLLER('/books')
 class BookController {
-  @HTTP_GET('/getall')
+  @HTTP_GET('/')
   public async getAllBooks(req: Request, res: Response) {
-    return res.status(200).json(await Book.find());
+    const sortOrder = req.query.sort;
+
+    if (! sortOrder) {
+      return res.status(200).json(await Book.find());
+    }
+    
+    res.status(200).json(await Book.getAllBooksSortedByReadingRate(sortOrder as string));
   }
 }
--- src/app/entities/Book.ts
+++ src/app/entities/Book.ts
@@ -1,7 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn, BaseEntity, ManyToMany, JoinTable, OneToMany, JoinColumn, Unique } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, BaseEntity, ManyToMany, JoinTable, OneToMany, JoinColumn, Unique, getManager } from 'typeorm';
 import { BookUser } from './BookUser';
-import { User } from './User';
-
 @Entity({
   name: 'books'
 })
@@ -39,4 +37,18 @@ export class Book extends BaseEntity {
     referencedColumnName: 'book_id'
   })
   junction: BookUser[]
+
+  public static async getAllBooksSortedByReadingRate(sortOrder: string = 'DESC'): Promise<Book[]> {
+    const pattern = new RegExp('(^asc$)|(^desc$)', 'i');
+    const order = pattern.test(sortOrder) ? sortOrder : 'DESC';
+    const entityManager = getManager();
+    let sql = `
+    SELECT books.*, COUNT(*) as reader_count
+    FROM books 
+    LEFT JOIN book_user ON book_user.book_id = books.id
+    GROUP BY books.id
+    ORDER BY reader_count {{SORT_ORDER}};`;
+    sql = sql.replace('{{SORT_ORDER}}', order);
+    return await entityManager.query(sql);
+  }
 }
