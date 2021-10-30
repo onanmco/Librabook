22023be128c6c5348663391e26936f37d79268ff
onanmco <onanmco@gmail.com>
2021-10-30 13:52:33 +0300

    [bugfix] sorting function was not working when books sorted by readers count

--- src/app/entities/Book.ts
+++ src/app/entities/Book.ts
@@ -1,13 +1,13 @@
-import { Entity, Column, PrimaryGeneratedColumn, BaseEntity, OneToMany, JoinColumn, Unique, getManager } from 'typeorm';
-import { Sortable } from '../types/Sortable';
+import { Entity, Column, PrimaryGeneratedColumn, BaseEntity, OneToMany, JoinColumn, Unique, getManager, getConnection } from 'typeorm';
 import { BookUser } from './BookUser';
 import * as utils from '../../utils';
 import { SortOrder } from '../types/QueryParams';
+import { User } from './User';
 @Entity({
   name: 'books'
 })
 @Unique(['content'])
-export class Book extends BaseEntity implements Sortable{
+export class Book extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
@@ -42,23 +42,27 @@ export class Book extends BaseEntity implements Sortable{
   junction: BookUser[]
 
   public static async getAllBooks(sortCriteria = null, sortOrder = null): Promise<Book[]> {
-    let select = 'SELECT books.*';
-    let from = ' FROM books';
+    const connection = await getConnection();
+    const tableName = utils.getTableName(connection, this);
+    const junctionTableName = utils.getJunctionTableName([ Book, User ]);
+    const columnNames = utils.getTableColumns(connection, this);
+    const allowedOrderKeywords = Object.keys(SortOrder).map(order => SortOrder[order]);
+
+    let select = `SELECT ${tableName}.*`;
+    let from = ` FROM ${tableName}`;
     let join = '';
     let groupBy = '';
     let orderBy = '';
 
-    const isSortCriteriaATableColumn = (await utils.getTableColumns(Book)).includes(sortCriteria);
-    const isValidOrderSpecified = Object.keys(SortOrder).map(o => SortOrder[o]).includes(sortOrder);
-
-    let params = [];
+    const isSortCriteriaATableColumn = columnNames.includes(sortCriteria);
+    const isValidOrderSpecified = allowedOrderKeywords.includes(sortOrder);
 
     if (isSortCriteriaATableColumn) {
       orderBy = ` ORDER BY ${sortCriteria} ${isValidOrderSpecified ? sortOrder : SortOrder.DESC}`;
     } else if (isValidOrderSpecified) {
       const aggregateCriteria = 'readers_count';
-      select = `${select}, COUNT(*) AS ${aggregateCriteria}`;
-      join = ' LEFT JOIN book_user ON book_user.book_id = books.id';
+      select = `${select}, COUNT(user_id) AS ${aggregateCriteria}`;
+      join = ` LEFT JOIN ${junctionTableName} ON ${junctionTableName}.book_id = books.id`;
       groupBy = ' GROUP BY books.id';
       orderBy = ` ORDER BY ${aggregateCriteria} ${sortOrder}`;
     }
