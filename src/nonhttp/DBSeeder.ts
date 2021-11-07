332bdc99d0d5f68fc46055e9050c90416cba0032
onanmco <onanmco@gmail.com>
2021-11-07 13:21:44 +0300

    [enhancement] db schema updated

--- src/nonhttp/DBSeeder.ts
+++ src/nonhttp/DBSeeder.ts
@@ -10,74 +10,72 @@ dotenv.config();
 export class DBSeeder {
   public static async seed(): Promise<void> {
     //seed roles to db
-    console.debug(`${new Date().toISOString()} - [DEBUG] DBSeeder -> seed(): Starting to insert all roles to the db.`);
-    Roles.getAllRoles().forEach(async (role) => {
-      console.debug(`${new Date().toISOString()} - [DEBUG] DBSeeder -> seed(): Starting to insert role '${role}' to the db.`);
-      try {
-        await Role.create({
-          name: role
-        }).save();
-      } catch (error) {
-        console.debug(`${new Date().toISOString()} - [DEBUG] DBSeeder -> seed(): Failed to insert role '${role}' to the db.`);
+    console.debug(`${new Date().toISOString()} - [DEBUG] DBSeeder -> seed(): Started to create roles.`);
+    try {
+      const roleNames = Roles.getAllRoles();
+
+      for (let i = 0; i < roleNames.length; i++) {
+        const role = roleNames[i];
+          const savedRole = Role.create({ name: role });
+          await savedRole.save();
       }
-    });
-    
+    } catch (error) {
+      console.debug(`${new Date().toISOString()} - [DEBUG] DBSeeder -> seed(): Started to create roles.`);
+    }
+
     //seed groups to db
-    console.debug(`${new Date().toISOString()} - [DEBUG] DBSeeder -> seed(): Starting to insert all groups to the db.`);
-    Groups.getAllGroups().forEach(async (group) => {
-      console.debug(`${new Date().toISOString()} - [DEBUG] DBSeeder -> seed(): Starting to insert group '${group}' to the db.`);
-      try {
-        await Group.create({
-          name: group
-        }).save();
-      } catch (error) {
-        console.debug(`${new Date().toISOString()} - [DEBUG] DBSeeder -> seed(): Failed to insert group '${group}' to the db.`);
+    console.debug(`${new Date().toISOString()} - [DEBUG] DBSeeder -> seed(): Started to create groups.`);
+    try {
+      const groupNames = Groups.getAllGroups();
+      for (let i = 0; i < groupNames.length; i++) {
+        const group = groupNames[i];
+          const savedGroup = Group.create({ name: group });
+          await savedGroup.save();
       }
-    });
+    } catch (error) {
+      console.debug(`${new Date().toISOString()} - [DEBUG] DBSeeder -> seed(): Failed to create groups.`);
+    }
 
     //associate roles with groups
-    console.debug(`${new Date().toISOString()} - [DEBUG] DBSeeder -> seed(): Starting to associate roles with groups on the db.`);
-    const groups = await Group.find();
-    
-    groups.forEach(async (group) => {
-      const rolesForCurrentGroup = [];
-      const roleNames = group.name == Groups.ROOT ? Roles.getRootRoles() : Roles.getConsumerRoles();
-      
-      roleNames.forEach(async (roleName) => {
-        const role = await Role.findOne({
-          where: {
-            name: roleName
-          }
-        });
-        rolesForCurrentGroup.push(role);
-      });
-      group.roles = rolesForCurrentGroup;
-      try {
-        await group.save();
-      } catch (error) {
-        console.debug(`${new Date().toISOString()} - [DEBUG] DBSeeder -> seed(): Failed to associate roles with group ${group.name} on the db.`);
-      }
-    });
-    
-    //create root users
-    console.debug(`${new Date().toISOString()} - [DEBUG] DBSeeder -> seed(): Starting to create root users.`);
-    const rootCredentials = JSON.parse(process.env.ROOT_CREDS);
-    
-    rootCredentials.forEach(async (cred) => {
-      console.debug(`${new Date().toISOString()} - [DEBUG] DBSeeder -> seed(): Starting to create root user with email ${cred.email}.`);
-      try {
-        const user = User.create({
-          email: cred.email,
-          password_hash: bcrypt.hashSync(cred.password, 10),
-          first_name: cred.first_name,
-          last_name: cred.last_name,
-          group_id: groups.filter(g => g.name === Groups.ROOT)[0].id,
-        });
+    console.debug(`${new Date().toISOString()} - [DEBUG] DBSeeder -> seed(): Started to associate roles with groups.`);
+    try {
+      const groups = await Group.find();
 
-        await user.save();
-      } catch (error) {
-        console.debug(`${new Date().toISOString()} - [DEBUG] DBSeeder -> seed(): Failed to create root user with email ${cred.email}.`);
+      for (let i = 0; i < groups.length; i++) {
+        const group = groups[i];
+        const rolesForCurrentGroup = [];
+        const roleNames = group.name == Groups.ROOT ? Roles.getAllRoles() : Roles.getConsumerRoles();
+
+        for (let j = 0; j < roleNames.length; j++) {
+          const role = await Role.findOne({ where: { name: roleNames[j] } });
+          rolesForCurrentGroup.push(role);
+        }
+        group.roles = rolesForCurrentGroup;
+          await group.save();
       }
-    });
+    } catch (error) {
+      console.debug(`${new Date().toISOString()} - [DEBUG] DBSeeder -> seed(): Failed to associate roles with groups.`);
+    }
+
+    //create root users
+    console.debug(`${new Date().toISOString()} - [DEBUG] DBSeeder -> seed(): Started to create root users.`);
+    try {
+      const rootCredentials = JSON.parse(process.env.ROOT_CREDS);
+      const groups = await Group.find();
+
+      for (let i = 0; i < rootCredentials.length; i++) {
+          const cred = rootCredentials[i];
+          const user = User.create({
+            email: cred.email,
+            password_hash: bcrypt.hashSync(cred.password, 10),
+            first_name: cred.first_name,
+            last_name: cred.last_name,
+            group_id: groups.filter(g => g.name === Groups.ROOT)[0].id
+          });
+          await user.save();
+      }
+    } catch (error) {
+      console.debug(`${new Date().toISOString()} - [DEBUG] DBSeeder -> seed(): Failed to create root users.`);
+    }
   }
 }
